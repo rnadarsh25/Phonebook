bcbd0385049f21c4b75880815e798d86ec4c5853
Adarsh Singh Tiwari <49534843+rnadarsh25@users.noreply.github.com>
2020-05-29 09:21:19 +0530

    Database connection

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect('localhost', 'root', '', 'phonebook');
+$connection = mysqli_connect('localhost', 'id13876421_adarsh25', 'adarshSinghtiwari@1234', 'id13876421_phonebook');
 
 if(!$connection){
     die("Connection Failed!".mysqli_error($connection));
